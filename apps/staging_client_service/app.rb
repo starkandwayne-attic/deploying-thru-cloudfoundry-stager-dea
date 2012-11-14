1c1bee1b28d08031c300337c3bd0e1ef65873ef1
Dr Nic Williams <drnicwilliams@gmail.com>
2012-11-14 10:02:42 -0800

    staging_client_service has endpoint to download droplets

--- apps/staging_client_service/app.rb
+++ apps/staging_client_service/app.rb
@@ -1,14 +1,36 @@
 require 'sinatra'
+require 'fileutils'
+
+droplet_dir = "/tmp/deploying-thru-cloudfoundry-stager-dea/droplets"
+FileUtils.mkdir_p(droplet_dir)
 
 get '/download_unstaged_app/:id' do
   p params
-  unstaged_app_tgz = File.expand_path("../../sinatra-app.zip", __FILE__)
-  send_file(unstaged_app_tgz)
+  unstaged_app_package = File.expand_path("../../sinatra-app.zip", __FILE__)
+  send_file(unstaged_app_package)
 end
 
 post '/upload_droplet/:id/:upload_id' do
   p params
   src_path = params[:upload][:droplet][:tempfile]
-  droplet = params[:upload][:droplet][:tempfile].read
   puts "stager uploaded staged droplet #{src_path}"
+  stored_droplet = File.join(droplet_dir, params[:id])
+  File.open(stored_droplet, "w") do |file|
+    file << params[:upload][:droplet][:tempfile].read
+  end
+  puts "droplet stored at #{stored_droplet}"
+end
+
+get '/staged_droplets/:id/:hash' do
+  p params
+  stored_droplet = File.join(droplet_dir, params[:id])
+  if File.exists?(stored_droplet)
+    puts "droplet found at #{stored_droplet}"
+    send_file(stored_droplet)
+  else
+    # From cloud_error.rb in cloud_controller
+    # HTTP_NOT_FOUND             = 404
+    # APP_NOT_FOUND = [301, HTTP_NOT_FOUND, "Application not found"]
+    404
+  end
 end
